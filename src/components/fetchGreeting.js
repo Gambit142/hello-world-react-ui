31bee4a334c0a74855d3356fbca2901c29e44d9b
Gambit142 <nzefrancismaryeugorji@gmail.com>
2022-02-16 20:53:37 +0100

    Install Stylelint in the project as a dependency

--- src/components/fetchGreeting.js
+++ src/components/fetchGreeting.js
@@ -2,7 +2,6 @@ const fetchGreeting = async () => {
   try {
     const response = await fetch('http://localhost:3000/api/greetings');
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     throw new Error(400);
